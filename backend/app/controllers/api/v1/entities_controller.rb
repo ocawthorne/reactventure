ffb07f70351a097ebe9ee6a87e2c9a86880d700d
ocawthorne <ocawthorne@gmail.com>
2021-02-22 04:41:09 +0000

    Exclude created at and updated at times for entities

--- backend/app/controllers/api/v1/entities_controller.rb
+++ backend/app/controllers/api/v1/entities_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::EntitiesController < ApplicationController
    def index
       entity = Entity.all
-      render json: entity
+      render json: entity, except: [:created_at, :updated_at]
    end
 end
