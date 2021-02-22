99fd4058c93ce4eb564e91713064bb3807912d95
ocawthorne <ocawthorne@gmail.com>
2021-02-22 00:59:32 +0000

    Amend routes to allow for login/signup pathways

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  post "api/v1/login", to: "api/v1/sessions#create"
+  get "api/v1/current_user", to: "api/v1/sessions#current_user"
+  post "/api/v1/signup", to: "api/v1/users#create"
+
   namespace :api do
     namespace :v1 do
       resources :entities
