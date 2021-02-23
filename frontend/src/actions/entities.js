41ea2120ed80228307829dc87ce215c31e819384
ocawthorne <ocawthorne@gmail.com>
2021-02-23 07:34:08 +0000

    Fix fetch

--- frontend/src/actions/entities.js
+++ frontend/src/actions/entities.js
@@ -1,24 +1,50 @@
-export const fetchEntitiesSuccess = (entities) => {
+export const fetchEntitiesSuccess = (allEntities) => {
+   console.log('Sending action to fetch entities.')
    return {
       type: "FETCH_ENTITIES_SUCCESS",
-      entities
+      allEntities
    }
-}
+} 
 
-//? Asynchronous Action Creators
+// export const getAllEntities = () => {
+//    console.log('Fetching entities...')
+//    // return dispatch => {
+//    //    return (
+//          fetch("http://localhost:3000/api/v1/entities", {
+//             method: "GET",
+//             headers: {
+//                "Content-Type": "application/json",
+//                "Access-Control-Allow-Origin": "*"
+//             }
+//          })
+//          .then(resp => resp.json())
+//          .then(allEntities => {
+//             console.log('About to dispatch fetchEntitiesSuccess')
+//             // return allEntities
+//          })
+//    //    )
+//    // }
+// }
 
 export const getAllEntities = () => {
    return dispatch => {
-      dispatch({type: "LOADING_ENTITIES"})
+      dispatch({type: 'LOADING_ENTITIES'})
       return (
          fetch("http://localhost:3000/api/v1/entities", {
-            method: "GET",
-            headers: { "Content-Type": "application/json" }
+         credentials: "include",
+         method: "GET",
+         headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": true
+         }
          })
-         .then(resp => resp.json())
+         .then(r => r.json())
          .then(entities => {
-            dispatch(fetchEntitiesSuccess(entities))
-         })
+            dispatch(fetchEntitiesSuccess(entities));
+            })
+         .catch(error => {
+            console.log("Error: ", error);
+            })
       )
    }
 }
