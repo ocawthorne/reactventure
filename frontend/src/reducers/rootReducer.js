6c6825a5f9f6cb8cb67d9ff8d17b937090f03ac3
ocawthorne <ocawthorne@gmail.com>
2021-02-23 07:35:29 +0000

    Remove redundant reducer from root

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import { commands } from './commands'
-import { entities } from './entities'
 import { logins } from './logins'
 
 export default combineReducers({
    commands,
-   entities,
    logins
 })
