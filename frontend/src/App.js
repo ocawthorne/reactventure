4c89002339de268451c443d29a4c3e3ad0fa82aa
ocawthorne <ocawthorne@gmail.com>
2021-02-23 07:33:43 +0000

    Move getAllEntities to App when mounted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React, {Component} from "react";
+import { connect } from 'react-redux';
+
 import './App.css';
 import EntryField from "./containers/EntryField";
 import SideBar from './components/SideBar'
 import History from './containers/History'
 
+import { getAllEntities } from './actions/entities'
+
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log('App mounted, fetching all entities.')
+    this.props.getAllEntities()
+ }
+
   render() {
     return (
       <div className="App">
@@ -19,4 +29,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+     allEntities: state.commands.allEntities
+  }
+}
+
+export default connect(mapStateToProps, { getAllEntities })(App)
