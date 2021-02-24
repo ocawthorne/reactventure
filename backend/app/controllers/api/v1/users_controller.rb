23275753d6b63b05cd4573e62b2ea38c6c390121
ocawthorne <ocawthorne@gmail.com>
2021-02-24 23:07:58 +0000

    Add create action to users controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -8,4 +8,13 @@ class Api::V1::UsersController < ApplicationController
       user = User.find_by_id(id: params[:id])
       render json: user
    end
+
+   def create
+      user = User.new(user_params)
+      render json: (user.save ? { user: user, logged_in: true } : { user: {}, logged_in: false, error: "There was a problem with your user creation." })
+   end
+
+   private
+
+   params.require(:user).permit(:username, :password, :password_confirmation)
 end
