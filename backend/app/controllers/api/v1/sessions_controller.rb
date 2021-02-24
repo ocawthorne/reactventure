7b7ba7963a7208f2a510af6f976120c2dd322343
ocawthorne <ocawthorne@gmail.com>
2021-02-24 23:25:17 +0000

    Fix session hash placement

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
    def create
       user = User.find_by(username: params[:user][:username])
-      session[:user_id] = user.id
       if user && user.authenticate(params[:user][:password])
-            render json: {user: user, logged_in: true}
+         session[:user_id] = user.id
+         render json: { user: user, logged_in: true }
       else 
-         render json: {
-            error: "Incorrect username or password."
-         }
+         render json: { error: "Incorrect username or password." }
       end 
    end 
 
