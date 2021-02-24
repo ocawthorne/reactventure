876eec8c1746effc6126e6dde7e0fc62cc6f77d1
ocawthorne <ocawthorne@gmail.com>
2021-02-24 23:00:02 +0000

    Creating option to log out

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
    def create
-      user = User.find_by(username: params[:session][:username])
+      user = User.find_by(username: params[:user][:username])
       session[:user_id] = user.id
-      if user && user.authenticate(params[:session][:password])
-            render json: user
+      if user && user.authenticate(params[:user][:password])
+            render json: {user: user, logged_in: true}
       else 
          render json: {
-            error: "Invalid Credentials"
+            error: "Incorrect username or password."
          }
       end 
    end 
+
+   def logout
+      reset_session
+      render json: { user: {}, logged_in: false}
+   end
 
    def current_user
       if logged_in?
