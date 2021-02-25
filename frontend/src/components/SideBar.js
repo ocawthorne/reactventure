c0ebbf6b80a672279c1e78514a1fcc7589efeb73
ocawthorne <ocawthorne@gmail.com>
2021-02-25 16:51:23 +0000

    Add router links to sign up etc

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 
 import logo from '../logo.svg';
 import Inventory from '../containers/Inventory'
@@ -12,8 +12,12 @@ export default function SideBar() {
          <img src={logo} className="App-logo" alt="logo" />
          <Inventory />
          <br />
+         <Link to="/" className="link">Adventure</Link>
+         <Link to="/signin" className="link">Sign In</Link>
+         <Link to="/signup" className="link">Sign Up</Link>
          <Switch>
-            <Route exact path="/" component={SignIn} />
+            <Route exact path="/" />
+            <Route exact path="/signin" component={SignIn} />
             <Route exact path="/signup" component={SignUp} />
          </Switch>
       </div>
