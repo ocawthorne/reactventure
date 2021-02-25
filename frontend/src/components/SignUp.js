c90ec27b9fd608c900b0a4d40ae457588186a57c
ocawthorne <ocawthorne@gmail.com>
2021-02-25 16:08:00 +0000

    Remove break and hero name from signup process

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -4,13 +4,10 @@ export default class SignUp extends React.Component {
    render() {
       return (
          <div className="session-details">
-            <br />
             <p>Start a new adventure</p>
             <form>
                <label htmlFor="username">Username</label>
                <input type="text" className="username field" /><br />
-               {/* <label htmlFor="hero">Heroname</label>
-               <input type="text" className="hero field" /><br /> */}
                <label htmlFor="password">Password</label>
                <input type="password" className="password field" /><br />
                <input type="submit" className="login-submit" value="Let's begin." />
