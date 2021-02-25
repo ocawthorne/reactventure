36567658535e5773533d67eaa1e2a8e089a5816f
ocawthorne <ocawthorne@gmail.com>
2021-02-25 18:12:06 +0000

    Add protection from forgery

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+   protect_from_forgery with: :exception
    skip_before_action :verify_authenticity_token
    helper_method :logged_in, :current_user
 
