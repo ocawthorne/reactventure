28d6d5886b15ae8c1b387bbdeea616860138aee0
ocawthorne <ocawthorne@gmail.com>
2021-02-25 18:12:33 +0000

    Fix fetch URL

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,6 +1,6 @@
 export const signup = (userData) => {
    return dispatch => {
-      fetch(`http://localhost:3000/users`, {
+      fetch(`http://localhost:3000/api/v1/users`, {
          method: 'POST',
          headers: {
             'Content-Type': 'application/json'
