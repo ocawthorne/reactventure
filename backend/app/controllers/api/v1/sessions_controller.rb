5eda294a4e645614656242cbffed1bb5f6e465a0
ocawthorne <ocawthorne@gmail.com>
2021-02-26 13:22:48 +0000

    Remove current_user method

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -6,21 +6,29 @@ class Api::V1::SessionsController < ApplicationController
          render json: { user: user, logged_in: true }
       else 
          render json: { error: "Incorrect username or password." }
-      end 
-   end 
+      end
+   end
+
+   def logged_in
+      if logged_in?
+         render json: { user: current_user, logged_in: true }
+      else
+         render json: { user: {}, logged_in: false }
+      end
+   end
 
    def logout
       reset_session
       render json: { user: {}, logged_in: false}
    end
 
-   def current_user
-      if logged_in?
-         render json: current_user 
-      else
-         render json: {
-            error: "Not logged in"
-         }
-      end
-   end
+   # def current_user
+   #    if logged_in?
+   #       render json: current_user 
+   #    else
+   #       render json: {
+   #          error: "Not logged in"
+   #       }
+   #    end
+   # end
 end
