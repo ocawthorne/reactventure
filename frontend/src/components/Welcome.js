1529eb2ed0f2ac73d0a0b9cbb1f019baec392e98
ocawthorne <ocawthorne@gmail.com>
2021-02-26 23:37:06 +0000

    Implement logout button

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { checkLoggedIn } from '../actions/auth'
+import { checkLoggedIn, logout, save, retrieve } from '../actions/auth'
 
 class Welcome extends React.Component {
+
+   saveProgress = () => {
+      console.log('About to save progress')
+      this.props.save(
+         this.props.user,
+         this.props.userHistory,
+         this.props.Inventory,
+         this.props.knownObjects,
+         this.props.brokenObjects
+      )
+   }
+
    render() {
-      console.log(this.props.loggedIn)
       return (
          <div>
             {this.props.loggedIn ? <p className="welcome">Welcome, {this.props.user.username}</p> : <p className="welcome">Welcome!<br />Please create an account to begin your adventure, or login to continue.</p>}
+            {this.props.loggedIn && <button className="entry submit" onClick={this.saveProgress}>Save Progress</button>}
+            <br /><br /><br />
+            {this.props.loggedIn && <button className="entry submit" onClick={() => this.props.logout()}>Log Out</button>}
          </div>
       )
    }
@@ -16,8 +30,13 @@ class Welcome extends React.Component {
 const mapStateToProps = state => {
    return {
       loggedIn: state.auth.loggedIn,
-      user: state.auth.currentUser
+      user: state.auth.currentUser,
+
+      userHistory: state.commands.userHistory,
+      inventory: state.commands.userObjects,
+      knownObjects: state.commands.knownObjects,
+      brokenObjects: state.commands.brokenObjects
    }
 }
 
-export default connect(mapStateToProps, { checkLoggedIn })(Welcome)
+export default connect(mapStateToProps, { checkLoggedIn, logout, save, retrieve })(Welcome)
