1732b74bd8ac4489cf55c2a0331a43dcae614b38
ocawthorne <ocawthorne@gmail.com>
2021-02-26 12:32:25 +0000

    Include ActionController::Cookies

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+   include ::ActionController::Cookies
    # protect_from_forgery with: :exception
    # skip_before_action :verify_authenticity_token
    # helper_method :logged_in, :current_user
