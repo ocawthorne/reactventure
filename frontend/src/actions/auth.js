25447a9dedbd6ed949e78d5867fe828bc80bf31c
ocawthorne <ocawthorne@gmail.com>
2021-02-26 17:50:51 +0000

    Add checkLoggedIn action

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -46,3 +46,30 @@ export const login = (userData, history) => {
       })
    }
 }
+
+export const checkLoggedIn = () => {
+   return dispatch => {
+      fetch(`http://localhost:3000/api/v1/logged_in`, {
+         credentials: 'include'
+      })
+      .then(res => res.json())
+      .then(data => dispatch({
+         type: "AUTH_SUCCESS",
+         payload: {
+            loggedIn: data.logged_in,
+            currentUser: data.user
+         }
+      }))
+   }
+}
+
+export const logout = () => {
+   return dispatch => {
+      fetch(`http://localhost:3000/api/v1/logout`, {
+         method: "DELETE",
+         credentials: "include"
+      })
+      .then(res => res.json())
+      .then(data => dispatch({type: "LOGOUT_SUCCESS"}))
+   }
+}
