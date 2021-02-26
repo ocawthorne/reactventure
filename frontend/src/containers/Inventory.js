cfadce3e38ef89c609e018d81467a09bfb6fed64
ocawthorne <ocawthorne@gmail.com>
2021-02-26 23:38:06 +0000

    Display blank inventory when not signed in

--- frontend/src/containers/Inventory.js
+++ frontend/src/containers/Inventory.js
@@ -11,19 +11,19 @@ class Inventory extends React.Component {
             <table>
                <tbody>
                   <tr>
-                     <td key="item_1">{this.props.userObjects[0]}</td>
-                     <td key="item_2">{this.props.userObjects[1]}</td>
-                     <td key="item_3">{this.props.userObjects[2]}</td>
+                     <td key="item_1">{this.props.user && this.props.userObjects[0]}</td>
+                     <td key="item_2">{this.props.user && this.props.userObjects[1]}</td>
+                     <td key="item_3">{this.props.user && this.props.userObjects[2]}</td>
                   </tr>
                   <tr>
-                     <td key="item_4">{this.props.userObjects[3]}</td>
-                     <td key="item_5">{this.props.userObjects[4]}</td>
-                     <td key="item_6">{this.props.userObjects[5]}</td>
+                     <td key="item_4">{this.props.user && this.props.userObjects[3]}</td>
+                     <td key="item_5">{this.props.user && this.props.userObjects[4]}</td>
+                     <td key="item_6">{this.props.user && this.props.userObjects[5]}</td>
                   </tr>
                   <tr>
-                     <td key="item_7">{this.props.userObjects[6]}</td>
-                     <td key="item_8">{this.props.userObjects[7]}</td>
-                     <td key="item_9">{this.props.userObjects[8]}</td>
+                     <td key="item_7">{this.props.user && this.props.userObjects[6]}</td>
+                     <td key="item_8">{this.props.user && this.props.userObjects[7]}</td>
+                     <td key="item_9">{this.props.user && this.props.userObjects[8]}</td>
                   </tr>
                </tbody>
             </table>
@@ -34,6 +34,7 @@ class Inventory extends React.Component {
 
 const mapStateToProps = state => {
    return {
+      user: state.auth.currentUser,
       userObjects: state.commands.userObjects
    }
 }
