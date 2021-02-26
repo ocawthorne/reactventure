c8d5d75600868237e3c33e994fb7f67c7c3d3045
ocawthorne <ocawthorne@gmail.com>
2021-02-26 23:35:15 +0000

    Add routes for getting and updating users

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post "/api/v1/users", to: "api/v1/users#create"
+  get "/api/v1/users/:id", to: "api/v1/users#show"
+  post "/api/v1/users/:id", to: "api/v1/users#update"
   get "api/v1/logged_in", to: "api/v1/sessions#logged_in"
   delete "api/v1/logout", to: "api/v1/sessions#logout"
 
