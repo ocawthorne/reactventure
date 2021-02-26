5189f633268bf5923a857488d4809aad09762375
ocawthorne <ocawthorne@gmail.com>
2021-02-26 23:37:48 +0000

    Relocate introduction to History component

--- frontend/src/containers/History.js
+++ frontend/src/containers/History.js
@@ -1,18 +1,23 @@
 import React, {Component} from "react"
 import { connect } from 'react-redux';
-import { commitHistoryToUser } from '../actions/users'
 
 class History extends Component {
-
-   componentDidUpdate() {
-      console.log('History updated.')
-      commitHistoryToUser(this.props.userHistory)
-      debugger
-   }
 
    render() {
       return (
          <div className="history">
+            <p>I've woken up in a strange, cold, dark little room with only a door in front of me.</p>
+            <p>There is a desk on the left of me with a lit candle on top of it and a piece of paper next to the candle.</p>
+            <p>An old chest sits to the right of me.</p>
+            <p>A crowbar lies in front of the door.</p>
+            <br />
+            <p>COMMANDS:</p>
+            <p>get/pick up/grab [item]" will add it to your inventory.</p>
+            <p>use [item] on [another object]" will apply the first object to the second.</p>
+            <p>look at [item]" will allow you to inspect it.</p>
+            <p>touch/feel [item]" will let you feel the object.</p>
+            <p>help" will display these choices again.</p>
+            <br />
             {this.props.userHistory.map(u => 
                u.split("\n").map(h => <p>{h}</p>)
             )}
@@ -23,8 +28,9 @@ class History extends Component {
 
 const mapStateToProps = state => {
    return {
-      userHistory: state.commands.userHistory,
+      currentUser: state.auth.currentUser,
+      userHistory: state.commands.userHistory
    }
 }
 
-export default connect(mapStateToProps, { commitHistoryToUser })(History)
+export default connect(mapStateToProps)(History)
