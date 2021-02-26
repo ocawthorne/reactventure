cd9bf7bc8424064b62a9867d88b8e5cf66e19f23
ocawthorne <ocawthorne@gmail.com>
2021-02-26 00:09:28 +0000

    Fix params in user creation

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
    def create
-      user = User.find_by(username: params[:user][:username])
-      if user && user.authenticate(params[:user][:password])
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
          session[:user_id] = user.id
          render json: { user: user, logged_in: true }
       else 
