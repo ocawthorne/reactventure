d9d47286ef347c094bd4ca00ab6e310df35e5575
ocawthorne <ocawthorne@gmail.com>
2021-02-26 13:22:33 +0000

    Add Helpers from ActionController

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::API
    include ::ActionController::Cookies
+   include ActionController::Helpers
    # protect_from_forgery with: :exception
    # skip_before_action :verify_authenticity_token
-   # helper_method :logged_in, :current_user
+   helper_method :logged_in?, :current_user
 
    private
 
    def current_user
-      current_user ||= User.find(id: session[:user_id]) if session[:user_id]
+      current_user ||= User.find(session[:user_id]) if session[:user_id]
    end 
 
    def logged_in?
