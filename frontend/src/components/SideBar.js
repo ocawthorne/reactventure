8305aa27c183fb11639395e6676b912ff51ded13
ocawthorne <ocawthorne@gmail.com>
2021-02-26 17:51:06 +0000

    Implement Welcome component

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -6,23 +6,27 @@ import logo from '../logo.svg';
 import Inventory from '../containers/Inventory'
 import SignUp from '../components/SignUp'
 import SignIn from '../containers/SignIn'
+import Welcome from '../components/Welcome'
+import { logout } from '../actions/auth'
 
-const SideBar = ({user}) => {
-   return (
-      <div className="sidebar">
-         <img src={logo} className="App-logo" alt="logo" />
-         <Inventory />
-         <br />
-         {user.username ? <p>Welcome, {user.username}!</p> : <Link to="/signup" className="link">New Adventure</Link>}
-         <br /><br />
-         {user.username ? <Link to="/signout">Log out</Link> : <Link to="/signin" className="link">Continue Adventure</Link>}
-         <Switch>
-            <Route exact path="/" />
-            <Route exact path="/signin" component={SignIn} />
-            <Route exact path="/signup" component={SignUp} />
-         </Switch>
-      </div>
-   )
+class SideBar extends React.Component {
+   render() {
+      return (
+         <div className="sidebar">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Inventory />
+            <br />
+            {this.props.user.username ? "" : <Link to="/signup" className="link">New Adventure</Link>}
+            <br /><br />
+            {this.props.user.username ? <Link to="" onClick={() => this.props.logout()}>Log out</Link> : <Link to="/signin" className="link">Continue Adventure</Link>}
+            <Switch>
+               <Route exact path="/" component={Welcome} />
+               <Route exact path="/signin" component={SignIn} />
+               <Route exact path="/signup" component={SignUp} />
+            </Switch>
+         </div>
+      )
+   }
 }
 
 const mapStateToProps = (state) => {
@@ -31,4 +35,4 @@ const mapStateToProps = (state) => {
    }
 }
 
-export default connect(mapStateToProps)(SideBar)
+export default connect(mapStateToProps, { logout })(SideBar)
