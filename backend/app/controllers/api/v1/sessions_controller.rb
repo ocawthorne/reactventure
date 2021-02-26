192561e759b77c7857761a40715a83b0008fd14b
ocawthorne <ocawthorne@gmail.com>
2021-02-26 12:32:44 +0000

    Adjust where information from params is obtained

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
    def create
-      user = User.find_by(username: params[:username])
-      if user && user.authenticate(params[:password])
+      user = User.find_by(username: params[:session][:username])
+      if user && user.authenticate(params[:session][:password])
          session[:user_id] = user.id
          render json: { user: user, logged_in: true }
       else 
