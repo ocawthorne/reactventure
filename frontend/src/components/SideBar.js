f692307d56242585295e65399d09293334777e72
ocawthorne <ocawthorne@gmail.com>
2021-03-02 14:54:27 +0000

    Change logo source and import About component

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -2,31 +2,32 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 
-import logo from '../logo.svg';
+import logo from '../reactventure.svg';
 import Inventory from '../containers/Inventory'
 import SignUp from '../components/SignUp'
 import SignIn from '../containers/SignIn'
 import Welcome from '../components/Welcome'
+import About from '../components/About'
 
 class SideBar extends React.Component {
    render() {
       return (
          <div className="sidebar">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo swingimage" alt="logo" />
             <Inventory />
             <br />
-            {!this.props.user.username && <Link to="/signup" className="link">New Adventure</Link>}
+            {!this.props.loggedIn && <Link to="/signup" className="link">New Adventure</Link>}
             <br /><br />
-            {!this.props.user.username && <Link to="/signin" className="link">Continue Adventure</Link>}
+            {!this.props.loggedIn && <Link to="/signin" className="link">Continue Adventure</Link>}
             <br /><br />
+            {!this.props.loggedIn && <Link to="/about" className="link">About</Link>}
+            <br />
             <Switch>
                <Route exact path="/" component={Welcome} />
                <Route exact path="/signin" component={SignIn} />
                <Route exact path="/signup" component={SignUp} />
+               <Route exact path="/about" component={About} />
             </Switch>
-            {/* <Welcome /> */}
-            {/* {this.props.user.username ? <Link to="" onClick={() => this.props.logout()}>Log out</Link> : <Link to="/signin" className="link">Continue Adventure</Link>} */}
-
          </div>
       )
    }
@@ -34,7 +35,7 @@ class SideBar extends React.Component {
 
 const mapStateToProps = (state) => {
    return {
-      user: state.auth.currentUser
+      loggedIn: state.auth.loggedIn
    }
 }
 
